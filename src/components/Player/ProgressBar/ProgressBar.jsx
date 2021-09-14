41ab8be78b0f9d98458688a3d42a2d61fce50b27
Marina Sirenko <sirenko.workspace@gmail.com>
2021-09-14 16:57:12 +0300

    исправить баг: при отсчете времени видео едет верстка

--- src/components/Player/ProgressBar/ProgressBar.jsx
+++ src/components/Player/ProgressBar/ProgressBar.jsx
@@ -5,8 +5,10 @@ function ProgressBar({ currentTime, duration, progressValue }) {
   return (
     <div className="player__progress-bar">
       <progress className="player__progress-line" max="100" value={progressValue}></progress>
-      <span className="player__video-duration">{currentTime}</span>
-      <span className="player__video-duration player__video-duration_type_full">{duration}</span>
+      <div className="player-wrap">
+        <p className="player__video-duration">{currentTime}</p>
+        <p className="player__video-duration player__video-duration_type_full">{duration}</p>
+      </div>
     </div>
   );
 }
